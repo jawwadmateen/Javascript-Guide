d1d5ec26477790a5b3a0a79a2fc21811d67e6090
Sheikh Jawwad Mateen <72332780+jawwadmateen@users.noreply.github.com>
2023-01-29 12:51:00 +0500

    Add files via upload

---
+++ Console Object Methods in JS.js
@@ -0,0 +1,19 @@
+console.log("HI jaanu") //used to for simple print statements
+console.error("Hey Error Lelo") //used to print errors in console
+console.assert(66>6666,"Wrong Assumption Brother") // used to print message if condition evaluates to false
+let a={
+    name:"Akram",
+    roll_Num:15,
+    section:"A"
+}
+console.table(a); //used to display object in tabular form in console
+console.warn("Please dont smoke"); //used to display warnings in console
+console.info("Important Info "); //used to display information (kind of similar to console.log() but info() is also displayed in info section)
+console.log(console)
+console.time("f1");
+for(let i=0;i>10;i++)
+{
+    console.log("Hi");
+}
+console.timeEnd("f1");  //console.time("f1") and console.timeEnd("f1") are used to find out the time elapsed bewtween execution of these two functions
+// There are numerous console object methods and some of them are above.You can search other on internet
