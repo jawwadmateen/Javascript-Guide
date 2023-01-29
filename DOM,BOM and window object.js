de000cbda544caf68902edc23a479fc81057ce54
Sheikh Jawwad Mateen <72332780+jawwadmateen@users.noreply.github.com>
2023-01-29 15:00:36 +0500

    Add files via upload

---
+++ DOM,BOM and window object.js
@@ -0,0 +1,8 @@
+//DOM,BOM and Window Object
+
+console.log(window)
+console.log(document.body) 
+document.body.style.background="red"
+//Document Object Model (DOM) converts the whole HTML page into a JS object known as document 
+//Browser Object Model (BOM) represents additional objects provided by the browser (host environment) for working with everything except the document
+//With the help of document object you can manipulate the html page using JS
