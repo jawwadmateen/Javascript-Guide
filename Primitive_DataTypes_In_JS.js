47847474a31220891de44ede2e72a204af963314
Sheikh Jawwad Mateen <72332780+jawwadmateen@users.noreply.github.com>
2023-02-10 18:52:42 +0500

    Add files via upload

---
+++ Primitive_DataTypes_In_JS.js
@@ -0,0 +1,17 @@
+
+//There are 7 Primitive DataTypes in JS
+let a=null; // NULL
+let b=444;  //Number
+let c=true   //Boolean 
+let d=BigInt("555")+BigInt("5") //BigInt
+let e=Symbol("I am Symbol")  //Symbol
+let f="John Cena is Good Wrestler" //String
+let g=undefined;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+
