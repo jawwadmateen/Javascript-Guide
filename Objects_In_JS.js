1e41921ae7a42526f85109fddacf1b5dcbcc14e0
Sheikh Jawwad Mateen <72332780+jawwadmateen@users.noreply.github.com>
2023-02-12 10:53:26 +0500

    Add files via upload

---
+++ Objects_In_JS.js
@@ -0,0 +1,14 @@
+//Objects in JS
+//Non-Primitive DataType (Key-Value Pair)
+const student_marks={
+    Jawwad:98,
+    Ali:55,
+    Wajih:88
+}
+console.log(student_marks.Jawwad) // refer to key and you will get the output 
+//OR
+console.log(student_marks["Wajih"]) //This is also a way of printing key's value
+
+
+
+
