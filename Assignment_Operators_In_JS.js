e705bcc58552aeee443d7a48598f6b80fd0ccbd5
Sheikh Jawwad Mateen <72332780+jawwadmateen@users.noreply.github.com>
2023-02-13 11:54:06 +0500

    Add files via upload

---
+++ Assignment_Operators_In_JS.js
@@ -0,0 +1,13 @@
+//Assignment  Operators in JS
+let a=10;
+a+=10;  //a=a+10
+console.log(a);
+a-=5;  //a=a-5
+console.log(a);
+a*=5;  //a=a*5
+console.log(a);
+a/=5;  //a=a/5
+console.log(a);
+a**=2;  //a=a**2
+console.log(a);
+
