460a7e281ec6ae4e7038cb4e40c76ef824df3f57
Sheikh Jawwad Mateen <72332780+jawwadmateen@users.noreply.github.com>
2023-02-13 11:52:45 +0500

    Add files via upload

---
+++ Arithmetic Operators in JS.js
@@ -0,0 +1,12 @@
+//Arithmetic Operators in JS
+
+let a=10;
+let b=14;
+let c=2;
+let d=4;
+console.log(a+b); //addition
+console.log(b-a);  //subtraction
+console.log(a*b);  //multiplication
+console.log(a/b);   //division
+console.log(c**d); //exponential operator
+console.log(d%c);  //modulus operator
