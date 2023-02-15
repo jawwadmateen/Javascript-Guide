770c256b924241eaed226e993fb552305cdaee3c
Sheikh Jawwad Mateen <72332780+jawwadmateen@users.noreply.github.com>
2023-02-15 21:27:01 +0500

    Add files via upload

---
+++ Comparison_Operators_In_JS.js
@@ -0,0 +1,14 @@
+//Comparison Operators in JS
+let a=18;
+let b=15;
+let c=18;
+let f=18
+let d="15";
+let e="18";
+console.log("a==b is",a==c); //as == compares both variable's value only 
+console.log("a===e is",a===e);  //as === compares both variable's value and datatype
+console.log("a>b is",a>b);  //true as a is grater than b
+console.log("a<b is",a<b);   //false as a is greater than b
+console.log("a>=f is",a>=f);  //true as a is equal to f
+console.log("a!=b is",a!=b); //true as a is not equal to b
+console.log("a!==e is",a!==e) //true as value is same but datatype is different 
---
+++ Pre_And_Post_Increment_In_JS.js
@@ -0,0 +1,8 @@
+//Pre Increment and Post-Increment in JS
+let a=10;
+console.log(++a); //11  increment then print updated variable
+console.log(a++); //11  print previous value and then updates variable
+console.log(--a);  //10  //decrements and then prints updated variable
+console.log(a--); //10  //prints previous value and then updates variable
+console.log(a); //10  
+console.log(a--); //10  //prints value and then updates variable
