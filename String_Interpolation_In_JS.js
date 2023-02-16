438a752093d18605cbe3dc0e48a73859c0d2dba9
Sheikh Jawwad Mateen <72332780+jawwadmateen@users.noreply.github.com>
2023-02-16 12:05:10 +0500

    Add files via upload

---
+++ String_Interpolation_In_JS.js
@@ -0,0 +1,8 @@
+//String Interpolation in JS
+let name="Aslam"
+console.log(name.length)
+let friend1="Maaz";
+let friend2='Sarim';
+let sentence=`${friend1} is a friend of ${friend2}`;
+console.log(sentence)
+//We can insert variables directly in template literal. This is called String Interpolation
