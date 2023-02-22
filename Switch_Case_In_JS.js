dc05bc476255870cc81056aa1b72b70f96545b82
Sheikh Jawwad Mateen <72332780+jawwadmateen@users.noreply.github.com>
2023-02-22 10:55:19 +0500

    Add files via upload

---
+++ Switch_Case_In_JS.js
@@ -0,0 +1,12 @@
+// Switch Case in JS
+let a=18;
+switch(a)
+{
+    case 10:
+        console.log("Ok");
+        break;  //Takes execution out of switch case
+    case 11:
+       console.log(" Not OK");
+    default:  //used for value other than described in above cases
+       console.log("Thanks for coming")  
+}
